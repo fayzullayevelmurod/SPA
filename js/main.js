add10338f9e3a8a451f48edae4ff49171c90247b
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-11-09 17:59:40 +0500

    treatmentsSwiper section the end

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ navLink.addEventListener('click', () => {
   navLink.classList.remove('active')
 });
 
+
+
+var swiper = new Swiper(".treatmentsSwiper", {
+  slidesPerView: 1.10,
+  spaceBetween: 28,
+  // centeredSlides: true,
+  // slidesPerView: 'auto',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3.6,
+      spaceBetween: 50,
+    },
+  },
+});
